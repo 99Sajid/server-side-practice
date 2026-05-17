cb50d905a3a326648a5be13ae1d7d0869c2966f8
Md. Shajid Rahman <shajiedrahman@gmail.com>
2026-05-17 23:50:56 +0600

    Building GET Method Operations

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -5,6 +5,9 @@ import { readProduct } from "../service/product.service";
 export const productController=(req: IncomingMessage ,res: ServerResponse)=>{
    const url=req.url;
    const method=req.method;
+   const urlParts=url?.split("/");
+   const id=urlParts && urlParts[1] ==='products'? Number(urlParts[2]): null;
+   console.log("this is the actual id",id)
    if(url === '/products' && method === 'GET'){
 //     const product={
 //         id:1,
@@ -16,8 +19,16 @@ export const productController=(req: IncomingMessage ,res: ServerResponse)=>{
    data: products
    }));
   
-   }
-  
+   }else if(method==='GET' && id !==null ){
+      const products = readProduct();
+      const product = products.find((p)=> p.id === id);
+      //console.log(product);
+      res.writeHead(200,{'content-type':"application/json"})
+      res.end(JSON.stringify({message: "Product retrived successfully",
+   data: product
+   })
+   );
+}
    
 
 }
